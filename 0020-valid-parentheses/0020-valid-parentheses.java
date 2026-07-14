a6ed298861cb8abe0bc7235cfeb7f0e063b488f4
Akash Kumar Sahoo <sahooakash6oo2@gmail.com>
2026-07-14 12:45:11 +0530

    Time: 3 ms (87%), Space: 43.5 MB (15.86%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,44 @@
+import java.util.*;
+
+class Solution {
+
+    public boolean isValid(String s) {
+        Stack<Character> st = new Stack<>();
+
+        for (char ch : s.toCharArray()) {
+
+            if (ch == '(' || ch == '[' || ch == '{') {
+                st.push(ch);
+            }
+            else {
+
+                if (st.isEmpty()) {
+                    return false;
+                }
+
+                if (ch == ')') {
+                    if (st.peek() != '(') {
+                        return false;
+                    }
+                    st.pop();
+                }
+
+                else if (ch == ']') {
+                    if (st.peek() != '[') {
+                        return false;
+                    }
+                    st.pop();
+                }
+
+                else if (ch == '}') {
+                    if (st.peek() != '{') {
+                        return false;
+                    }
+                    st.pop();
+                }
+            }
+        }
+
+        return st.isEmpty();
+    }
+}
