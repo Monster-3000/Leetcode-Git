b42d5cd0e63421a2b5133fe999daf8739e608c8f
Akash Kumar Sahoo <sahooakash6oo2@gmail.com>
2026-08-30 13:39:16 +0530

    Time: 112 ms (98.08%), Space: 111.3 MB (69.71%) - LeetHub

---
+++ 0295-find-median-from-data-stream/0295-find-median-from-data-stream.java
@@ -0,0 +1,39 @@
+class MedianFinder {
+
+    PriorityQueue<Integer> left =
+        new PriorityQueue<>((a, b) -> b - a); // Max Heap
+
+    PriorityQueue<Integer> right =
+        new PriorityQueue<>();                // Min Heap
+
+    public MedianFinder() {
+    }
+
+    public void addNum(int num) {
+
+        // Put number into left first
+        if (left.isEmpty() || num <= left.peek()) {
+            left.add(num);
+        } else {
+            right.add(num);
+        }
+
+        // Balance the heaps
+        if (left.size() > right.size() + 1) {
+            right.add(left.poll());
+        }
+
+        if (right.size() > left.size()) {
+            left.add(right.poll());
+        }
+    }
+
+    public double findMedian() {
+
+        if (left.size() > right.size()) {
+            return left.peek();
+        }
+
+        return (left.peek() + right.peek()) / 2.0;
+    }
+}
