54b76b9194d09e30fd7a1d15ce856c9408e698a5
Akash Kumar Sahoo <sahooakash6oo2@gmail.com>
2026-09-13 12:57:05 +0530

    Time: 47 ms (58.85%), Space: 115.8 MB (56.73%) - LeetHub

--- 0435-non-overlapping-intervals/0435-non-overlapping-intervals.java
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.java
@@ -5,14 +5,19 @@ class Solution {
         int prevEnd = intervals[0][1];
     
         for(int i=1;i<intervals.length;i++){
+
             if(intervals[i][0]<prevEnd){
                 count++;
                 prevEnd=Math.min(prevEnd,intervals[i][1]);
             }
+
             else{
                 prevEnd=intervals[i][1];
             }
+            
         }
+
     return count;
+
     }
 }
