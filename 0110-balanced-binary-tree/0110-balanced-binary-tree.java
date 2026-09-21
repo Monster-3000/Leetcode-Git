72a4f3695ed8f4fed9b80bd91fb0664d98eea9c9
Akash Kumar Sahoo <sahooakash6oo2@gmail.com>
2026-09-21 10:15:42 +0530

    Time: 0 ms (100%), Space: 45.5 MB (77.13%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.java
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.java
@@ -21,14 +21,17 @@ class Solution {
         if(root == null){
             return 0;
         }
+
         int leftHeight = checkHeight(root.left);
         if(leftHeight==-1){
             return -1;
         }
+
         int rightHeight = checkHeight(root.right);
         if(rightHeight==-1){
             return -1;
         }
+
         if(Math.abs(leftHeight-rightHeight)>1){
             return -1;
         }
